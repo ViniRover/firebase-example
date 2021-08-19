814023e4959895898f42ce30a6ae4e67d914df83
ViniRover <viniciusrover3@gmail.com>
2021-08-19 11:10:37 -0300

    Check as answered service, check as highlighted service

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -9,6 +9,8 @@ import { Question } from '../components/Question';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 
 import '../styles/room.scss';
 import { useAuth } from '../hooks/useAuth';
@@ -56,6 +58,40 @@ export function AdminRoom() {
     }
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    const authorId = await database.ref(`rooms/${roomId}`).get().then(room => {
+      return room.val().authorId;
+    });
+
+    if(authorId !== user?.id) {
+      alert('You do not have permission to check this question');
+      return;
+    }
+
+    if(window.confirm('Do you wish to check this question as answered?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isAnswered: true,
+      });
+    }
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    const authorId = await database.ref(`rooms/${roomId}`).get().then(room => {
+      return room.val().authorId;
+    });
+
+    if(authorId !== user?.id) {
+      alert('You do not have permission to highlight this question');
+      return;
+    }
+
+    if(window.confirm('Do you wish to highlight this question?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isHighlighted: true,
+      });
+    }
+  }
+  
   return(
     <div id="page-room">
       <header>
@@ -80,7 +116,25 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
             >
+              {!question.isAnswered && (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                  >
+                    <img src={checkImg} alt="Check question as answered" />
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleHighlightQuestion(question.id)}
+                  >
+                    <img src={answerImg} alt="Highlight the question" />
+                  </button>
+                </>
+              )}
               <button
                 type="button"
                 onClick={() => handleDeleteQuestion(question.id)}
